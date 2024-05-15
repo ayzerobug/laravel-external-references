cfcfcdbae54979c87d7fa4c6ac587d39c0fcae09
Ayzerobug <aydcoder@gmail.com>
2024-05-15 11:23:50 +0100

    Added support for Laravel 11

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Ayzerobug\\LaravelExternalReferences\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Ayzerobug\\LaravelExternalReferences\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
@@ -28,9 +28,8 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-external-references_table.php.stub';
+
+        $migration = include __DIR__ . '/../database/migrations/create_external_references_table.php.stub';
         $migration->up();
-        */
     }
 }
