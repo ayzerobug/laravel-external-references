354d65ce6946699639120a11e20357be90336b0a
Ayzerobug <aydcoder@gmail.com>
2024-05-15 10:47:31 +0100

    Prepare for publishing

--- src/LaravelExternalReferencesServiceProvider.php
+++ src/LaravelExternalReferencesServiceProvider.php
@@ -9,11 +9,6 @@ class LaravelExternalReferencesServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-external-references')
             ->hasMigration('create_external_references_table');
